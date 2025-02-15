fd20d32018b69d37711fe593168ab86e0d1fdcdc
HRashmika <hirushiariyapala@gmail.com>
2025-02-15 20:09:25 +0530

    Login Page

--- Application/dr-val-app/src/App.js
+++ Application/dr-val-app/src/App.js
@@ -1,19 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import Login from "./Components/Login"; // Adjust path if needed
 
 function App() {
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    fetch("http://127.0.0.1:5000/api/data")  // Flask API URL
-      .then((response) => response.json())
-      .then((data) => setData(data.message))
-      .catch((error) => console.error("Error fetching data:", error));
-  }, []);
-
   return (
     <div>
-      <h1>React Frontend</h1>
-      <p>{data ? data : "Loading..."}</p>
+      <Login />
     </div>
   );
 }
