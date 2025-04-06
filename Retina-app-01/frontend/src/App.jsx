8286c378da819bb524dbe13e93b1571e71a546c4
HRashmika <hirushiariyapala@gmail.com>
2025-04-06 16:06:54 +0530

    Application component

--- Retina-app-01/frontend/src/App.jsx
+++ Retina-app-01/frontend/src/App.jsx
@@ -1,16 +1,26 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import DiabeticRetinopathy from "./assets/Stage";
-import Profile from "./assets/Profile";
-import Login from "./assets/Login";
-import Signup from "./assets/Signup";
+import Home from "./components/Home";
+import StagePage from "./components/Stage";
+import Chatbot from "./components/Chatbot";
+import Directions from "./components/Direction";
+import AboutUs from "./components/AboutUs";
+
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Blog from "./components/Blog"; // ✅ Blog component import
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<DiabeticRetinopathy />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/stage" element={<StagePage />} />
+        <Route path="/chatbot" element={<Chatbot />} />
+        <Route path="/direction" element={<Directions />} />
+        <Route path="/aboutus" element={<AboutUs />} />
+        <Route path="/blog" element={<Blog />} /> 
+      
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
